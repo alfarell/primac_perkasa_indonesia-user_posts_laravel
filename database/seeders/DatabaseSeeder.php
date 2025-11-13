b05239775a838f9f8d8693a8288bad29f2e67ec3
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2025-11-14 03:12:32 +0700

    feat: update db seeds for users and posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-        Post::factory(50)->create();
+        User::factory(30)->create();
+        Post::factory(60)->create();
     }
 }
